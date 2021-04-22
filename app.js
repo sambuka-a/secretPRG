74a7242104cbd585e9dd86a4b87b301085352c1a
Sambuka <vova.kurtiak@gmail.com>
2021-04-22 22:06:01 +0200

    level_4 Salting

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const { stringify } = require('qs');
-const encrypt = require('mongoose-encryption');
-
 const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -21,8 +23,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -40,17 +40,20 @@ app.get('/register', function (req, res) {
 });
 
 app.post('/register', function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        }); 
     });
 });
 
@@ -63,9 +66,11 @@ app.post('/login', function (req, res) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render('secrets');
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render('secrets');
+                    }
+                });
             }
         }
     });
